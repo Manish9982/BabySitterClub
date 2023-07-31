e193c0857571e4afadb97210a40816e5947d4553
Gaurav <gauravch@outsourcingservicesusa.com>
2023-07-31 16:19:03 +0530

    Register screen

--- src/components/TextInputComponent.js
+++ src/components/TextInputComponent.js
@@ -1,6 +1,7 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { TextInput } from 'react-native-paper'
+import Spaces from '../helper/Spaces'
 
 const TextInputComponent = ({ value, onChangeText, placeholder }) => {
   return (
@@ -19,6 +20,6 @@ export default TextInputComponent
 const styles = StyleSheet.create({
   input:
   {
-
+    marginVertical: Spaces.med
   }
 })
